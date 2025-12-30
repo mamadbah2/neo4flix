d18fe3e424c66058404fba673ca77a671f548715
mamadbah <bahmamadoubobosewa@gmail.com>
2025-12-30 11:34:15 +0000

    feat: Initialize recommendation and user services with Spring Boot
    - Added pom.xml for recommendation-service with dependencies for Spring Boot, Neo4j, and testing.
    - Created main application class for recommendation-service.
    - Configured application.yaml for recommendation-service with server port and Neo4j connection details.
    - Added test class for recommendation-service to verify context loading.
    
    - Added pom.xml for user-service with dependencies for Spring Boot, Neo4j, and OAuth2 resource server.
    - Created main application class for user-service.
    - Configured application.yaml for user-service with server port and Neo4j connection details.
    - Added test class for user-service to verify context loading.
    
    - Included Maven wrapper files and Dockerfile for both services to facilitate builds and deployments.
    - Added .gitignore and .gitattributes for user-service to manage ignored files and line endings.
    [FIRST COMMIT]

---
+++ backend/rating-service/src/main/java/sn/dev/rating_service/RatingServiceApplication.java
@@ -0,0 +1,13 @@
+package sn.dev.rating_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingServiceApplication.class, args);
+	}
+
+}
---
+++ backend/rating-service/src/test/java/sn/dev/rating_service/RatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.dev.rating_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
