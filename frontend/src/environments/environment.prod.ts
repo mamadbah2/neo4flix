562309be28e63e7400adcd06bea145b3e8a67a11
mamadbah <bahmamadoubobosewa@gmail.com>
2026-01-11 14:42:04 +0000

    feat(env): update Keycloak URLs to use HTTPS for production environment ======================= Grosse faille de secu pour mon clientSecret dans env prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   apiUrl: 'https://your-backend-url.vercel.app', // TODO: Remplacer par votre URL backend déployée
   keycloak: {
-    url: 'http://keycloak4flix.duckdns.org', // Votre Keycloak est déjà accessible publiquement
+    url: 'https://keykloak.freeddns.org', // Votre Keycloak est déjà accessible publiquement
     realm: 'neo4flix',
     clientId: 'neo4flix-app',
     clientSecret: 'x4I746Xkmejuj7zi7AC4WJh1DPk97Tyu', // ⚠️ Ne jamais exposer en production côté client
-    tokenEndpoint: 'http://keycloak4flix.duckdns.org/realms/neo4flix/protocol/openid-connect/token',
-    adminUrl: 'http://keycloak4flix.duckdns.org/admin/realms/neo4flix'
+    tokenEndpoint: 'https://keykloak.freeddns.org/realms/neo4flix/protocol/openid-connect/token',
+    adminUrl: 'https://keykloak.freeddns.org/admin/realms/neo4flix'
   }
 };
